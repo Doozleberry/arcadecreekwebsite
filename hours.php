a63e058439f10d9f3a06ecdaa9e60f5198cd877f
Shawn Shacterman <shawnshact@msea.us>
2015-12-03 13:56:13 -0800

    initial commit

---
+++ hours.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Hours</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<link href="styles/style.css" rel="stylesheet" type="text/css" />
+<link href="styles/layout.css" rel="stylesheet" type="text/css" />
+<script src="maxheight.js" type="text/javascript"></script>
+
+<script src="scripts/flash/AC_RunActiveContent.js" type="text/javascript"></script>
+
+
+<style>
+	.hoursContainer
+	{
+		width="1000";
+		height="600";
+		overflow:hidden;
+	}
+</style>
+</head>
+<body id="page7" onload="new ElementMaxHeight()">
+<div class="top_bg">
+<div class="top_bg2">
+<div id="main1">
+	<!-- header  -->
+	<div id="header2">
+		<iframe src="smallheader/smallheader.html" width="900" height="600" style="border:0;" frameborder="0"></iframe>
+	</div>
+	<div class="hoursContainer">
+				<iframe src="creekHours/index.php" width="1000" height="600" frameborder="0"></iframe>
+	</div>
+</div>
+</div>
+</div>
+<div id="main2"> 
+	<div class="row_2">
+	  <div class="column_3"></div>
+  </div>
+</div>
+<!-- footer -->
+
+<div id="footer_main">
+
+	<div id="footer">
+
+	<p align="right"> Arcade Creek Project &copy; 2014 | <a href="privacy.html">Privacy Policy</a>	</p><br />
+	<p> <a href="index.html">Home</a> | <a href="aboutus.html">About Us</a> | <a href="studies.html">Studies</a> | <a href="calendar.html">Calendar</a> | 
+		<a href="forstudents.html">For Students</a> | <a href="recognition.html">Recognition</a> | <a href="contactus.html">Contact Us</a></div>	
+
+</div>
+</body>
+</html>
